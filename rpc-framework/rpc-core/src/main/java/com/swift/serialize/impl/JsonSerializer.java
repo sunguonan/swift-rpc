c5f7a442a9802cb1a949c8d579741a3c21a646a5
孙国楠 <1559039169@qq.com>
2023-07-22 18:51:47 +0800

    feature: 拓展Json的序列化方式

--- rpc-framework/rpc-core/src/main/java/com/swift/serialize/impl/JsonSerializer.java
+++ rpc-framework/rpc-core/src/main/java/com/swift/serialize/impl/JsonSerializer.java
@@ -1,19 +1,35 @@
 package com.swift.serialize.impl;
 
+import com.alibaba.fastjson2.JSON;
 import com.swift.serialize.Serializer;
+import lombok.extern.slf4j.Slf4j;
 
 /**
+ * 使用JSON作为序列化  会出现一定的问题 所以我们只做出实现 并不关心具体效果
+ *
  * @author sunGuoNan
  * @version 1.0
  */
+@Slf4j
 public class JsonSerializer implements Serializer {
     @Override
     public byte[] serialize(Object object) {
-        return new byte[0];
+
+        if (object == null) {
+            return null;
+        }
+        byte[] result = JSON.toJSONBytes(object);
+        log.debug("对象【{}】已经完成了序列化操作，序列化后的字节数为【{}】", object, result.length);
+        return result;
     }
 
     @Override
     public <T> T deserialize(byte[] bytes, Class<T> clazz) {
-        return null;
+        if (bytes == null || clazz == null) {
+            return null;
+        }
+        Object object = JSON.parseObject(bytes, clazz);
+        log.debug("类【{}】已经完成了反序列化操作.", clazz);
+        return (T) object;
     }
 }
